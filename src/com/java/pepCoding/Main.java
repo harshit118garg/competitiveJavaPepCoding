b14ded61295d2f26c6499b49682c51d11f08099f
Harshit Garg <harshit118garg@gmail.com>
2022-05-02 09:59:21 +0530

    Competitive Coding Intro

---
+++ src/com/java/pepCoding/Main.java
@@ -0,0 +1,11 @@
+package com.java.pepCoding;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Competitive Coding");
+        System.out.println("Coding in Java");
+        System.out.println("Source : PepCoding");
+    }
+}
